c9b91a03bf20595a40c6429f521d0ea41eda48ef
0932aki <ogumitsu11@gmail.com>
2021-11-10 13:09:01 +0900

    1st

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 21 11:54:16 2021
+
+@author: OguraMitsuaki
+"""
+
+import streamlit as st
+
+
+st.title('初めてのstreamlit')
+st.write('私の名前は〇〇です')
+
+
+
+
